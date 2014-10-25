da062f60123c73217533e416ad9208cfbfdab7d0
鬼彻 <guiche.zj@alibaba-inc.com>
2014-10-25 15:32:11 +0800

    添加属性

--- branch2.java
+++ branch2.java
@@ -1,6 +1,11 @@
 package com.taobao.search
 
 class Branch2{
+	
+	private long id;
+	private String name;
+	private String property;
+
 	public static void main(String str[]){
 		System.out.println("Hello world");
 	}
