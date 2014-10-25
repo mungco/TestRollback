a015c01e9f0737f71b6a401dd5f4db0d15248fa3
鬼彻 <guiche.zj@alibaba-inc.com>
2014-10-25 15:42:40 +0800

    Revert "添加属性"
    This reverts commit da062f60123c73217533e416ad9208cfbfdab7d0.

--- branch2.java
+++ branch2.java
@@ -1,11 +1,6 @@
 package com.taobao.search
 
 class Branch2{
-	
-	private long id;
-	private String name;
-	private String property;
-
 	public static void main(String str[]){
 		System.out.println("Hello world");
 	}
